d859a409ba6cb4700e8ec1cfd17d11ad58410f54
MATHN3RD23 <emoolly44@gmail.com>
2025-11-29 12:06:12 -0600

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,4 +1,7 @@
-class Logic(QMainWindow, Ui_Test10):
+from PyQt6.QtWidgets import *
+from gui import *
+
+class Logic(QMainWindow, Ui_TVremoteWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
