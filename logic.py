d112b4576417a789cb2d8eb26f47c6a3f33b2f84
MATHN3RD23 <emoolly44@gmail.com>
2025-11-29 13:28:31 -0600

    tv logic

--- logic.py
+++ logic.py
@@ -1,7 +1,55 @@
 from PyQt6.QtWidgets import *
 from gui import *
+from television import *
+
+class CoolTelevision(Television):
+    def __init__(self):
+        super().__init__()
+        self.__settings = False
+        self.__channelList = False
+
+    def channel_up(self) -> None:
+        super().channel_down()
+        if self.__channelList:
+            self.__channelList = False
+
+    def channel_down(self) -> None:
+        super().channel_down()
+        if self.__channelList:
+            self.__channelList = False
+
+    def power(self) -> None:
+        super().power()
+        if self.__channelList:
+            self.__channelList = False
+        if self.__settings:
+            self.__settings = False
+
+    def settings(self) -> None:
+        if not self.__settings:
+            self.__settings = True
+
+    def channel_list(self) -> None:
+        if not self.__channelList:
+            self.__channelList = True
+
+
+    def exit(self) -> None:
+        if self.__channelList or self.__settings:
+            self.__channelList = False
+            self.__settings = False
+
+#separating TV logic and Gui logic
+
 
 class Logic(QMainWindow, Ui_TVremoteWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        tenna = Television()
+
+    def update(self):
+        #updates the visual gui
+        pass
+
---
+++ television.py
@@ -0,0 +1,79 @@
+"""
+Imported from Lab 12
+"""
+
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self) -> None:
+        """
+        Method initializes the television
+        """
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+
+    def power(self)-> None:
+        """
+        Method switches the current power status
+        """
+        self.__status = not self.__status
+
+    def mute(self)-> None:
+        """
+        Method switches the current muted status
+        """
+        if self.__status:
+            self.__muted = not self.__muted
+
+    def channel_up(self)-> None:
+        """
+        Method raises the current channel
+        """
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+
+    def channel_down(self)-> None:
+        """
+        Method lowers the current channel
+        """
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+               self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
+    def volume_up(self)-> None:
+        """
+        Method raises the current volume
+        """
+        if self.__status:
+            self.__muted = False
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+
+    def volume_down(self)-> None:
+        """
+        Method lowers the current volume
+        """
+        if self.__status:
+            self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+
+    def __str__(self) -> str:
+        """
+        Method prints out the current status of the television
+        :return: tv status
+        """
+        if self.__muted:
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}"
+        else:
+            return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
